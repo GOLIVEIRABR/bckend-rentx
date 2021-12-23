191a172833e9b52c60a88574755e2ccbab6faa1f
Geazi Oliveira <geazioliveira@hotmail.com>
2021-12-22 22:54:57 -0300

    Implementada a utilização do singleton do tsyringe

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,13 +1,13 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+// import { CreateCategoryController } from "./CreateCategoryController";
+// import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-export default (): CreateCategoryController => {
-  const categoriesrepository = new CategoriesRepository();
-  const createCategoryUseCase = new CreateCategoryUseCase(categoriesrepository);
-  const createCategoryController = new CreateCategoryController(
-    createCategoryUseCase
-  );
+// export default (): CreateCategoryController => {
+//   const categoriesrepository = new CategoriesRepository();
+//   const createCategoryUseCase = new CreateCategoryUseCase(categoriesrepository);
+//   const createCategoryController = new CreateCategoryController(
+//     createCategoryUseCase
+//   );
 
-  return createCategoryController;
-};
+//   return createCategoryController;
+// };
